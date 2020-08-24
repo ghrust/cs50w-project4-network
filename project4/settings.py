5d4e5064ab49bd6ac31e2b3601c5fbcba45797f7
ghrust <garaev.rustam.h@gmail.com>
2020-08-24 20:21:47 +0400

    update configs

--- project4/settings.py
+++ project4/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', default=True)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'cs50w-project4-network.herokuapp.com']
 
