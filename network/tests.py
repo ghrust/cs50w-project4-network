f9fd9bed575e1d31805830898782b321f0bd945c
ghrust <garaev.rustam.h@gmail.com>
2020-08-24 20:44:35 +0400

    test index view status code

--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class NetworkTestCase(TestCase):
+    """Tests for Network app."""
+
+    def test_index_page(self):
+        """Test index page."""
+
+        c = Client()
+        response = c.get('/')
+
+        self.assertEqual(response.status_code, 200)
