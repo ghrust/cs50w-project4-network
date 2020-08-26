7abff5d5a02f33fc4fa13c6f260fb59ee727a0ef
ghrust <garaev.rustam.h@gmail.com>
2020-08-26 19:12:49 +0400

    test index login logout

--- network/tests.py
+++ network/tests.py
@@ -1,8 +1,17 @@
 from django.test import TestCase, Client
+from .models import User
 
-# Create your tests here.
+
 class NetworkTestCase(TestCase):
     """Tests for Network app."""
+
+    def setUp(self):
+        # Create test user.
+        user = User.objects.create_user(
+            username='user1',
+            password='pass',
+            email='user1@testmail.com'
+        )
 
     def test_index_page(self):
         """Test index page."""
@@ -11,3 +20,40 @@ class NetworkTestCase(TestCase):
         response = c.get('/')
 
         self.assertEqual(response.status_code, 200)
+
+    def test_get_request_login_page(self):
+        """Test login page."""
+
+        response = Client().get('/login')
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_request_login_page_valid_username_and_password(self):
+        """
+        Test post request for login page
+        if username and passowrd is valid.
+        """
+
+        response = Client().post(
+            '/login',
+            {'username': 'user1', 'password': 'pass'}
+        )
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_request_login_page_invalid_username(self):
+        """Test post request for login if username is invalid."""
+
+        response = Client().post(
+            '/login',
+            {'username': 'user2', 'password': 'pass'}
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout(self):
+        """Test logout view."""
+
+        response = Client().get('/logout')
+
+        self.assertEqual(response.status_code, 302)
